a88f9bcf0ee6394f90c6e3da24b621417ed5b757
Alzubair98 <203721133@ogr.altinbas.edu.tr>
2022-10-24 19:38:45 +0300

    add the controllers and actions for users

---
+++ app/controllers/user_posts_controller.rb
@@ -0,0 +1,7 @@
+class UserPostsController < ApplicationController
+    def show 
+    end
+
+    def index 
+    end
+end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,3 +1,16 @@
 class UsersController < ApplicationController 
+    def index 
+        @users = User.all
+    end
+    
+    def show
+        find_user
+    end
 
+
+    private
+
+    def find_user 
+        @user = User.find(params[:id])
+    end
 end
