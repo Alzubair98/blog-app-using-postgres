849d311ab5975e99cac501529109bb0a71d554d7
Alzubair98 <203721133@ogr.altinbas.edu.tr>
2022-10-26 01:52:27 +0300

    fix linters

--- spec/request/post_controller_spec.rb
+++ spec/request/post_controller_spec.rb
@@ -1,32 +1,32 @@
-require_relative '../rails_helper.rb'
+require_relative '../rails_helper'
 
-RSpec.describe "post controller", :type => :request do
-    context "the index method" do
-        it "show all users" do
-            get("/users/10/posts")
-            expect(assigns(:posts)).to eq(Post.all)  
-        end
-
-        it "render index template" do
-            get("/users/10/posts")
-            expect(response).to render_template('index')  
-        end
-    
-        it "the veiw body is correct (index)" do
-            get("/users/10/posts")
-            expect(response.body).to include('<h1>index for user posts</h1>')
-        end
+RSpec.describe 'post controller', type: :request do
+  context 'the index method' do
+    it 'show all users' do
+      get('/users/10/posts')
+      expect(assigns(:posts)).to eq(Post.all)
     end
 
-    context "the show method" do
-        it 'show specific user post veiw' do
-            get("/users/403/posts/85")
-            expect(response.body).to include('<h1>spesific user post</h1>')  
-        end
-
-        it 'render the correct page' do
-            get('/users/403/posts/85')
-            expect(response.body).to render_template('show')  
-        end
+    it 'render index template' do
+      get('/users/10/posts')
+      expect(response).to render_template('index')
     end
+
+    it 'the veiw body is correct (index)' do
+      get('/users/10/posts')
+      expect(response.body).to include('<h1>index for user posts</h1>')
+    end
+  end
+
+  context 'the show method' do
+    it 'show specific user post veiw' do
+      get('/users/403/posts/85')
+      expect(response.body).to include('<h1>spesific user post</h1>')
+    end
+
+    it 'render the correct page' do
+      get('/users/403/posts/85')
+      expect(response.body).to render_template('show')
+    end
+  end
 end
--- spec/request/users_controller_spec.rb
+++ spec/request/users_controller_spec.rb
@@ -1,32 +1,32 @@
-require_relative '../rails_helper.rb'
+require_relative '../rails_helper'
 
-RSpec.describe "user controller", :type => :request do
-    context "the index method" do
-        it "show all users" do
-            get("/users")
-            expect(assigns(:users)).to eq(User.all)  
-        end
-    
-        it "render index template" do
-            get("/users")
-            expect(response).to render_template('index')  
-        end
-    
-        it "the veiw body is correct (index)" do
-            get('/users')
-            expect(response.body).to include('<h1>index for users</h1>')
-        end
+RSpec.describe 'user controller', type: :request do
+  context 'the index method' do
+    it 'show all users' do
+      get('/users')
+      expect(assigns(:users)).to eq(User.all)
     end
-    
-    context "the show method" do
-        it 'show specific user veiw' do
-            get('/users/403')
-            expect(response.body).to include('<h1>Here is a spesific user</h1>')  
-        end
 
-        it 'render the correct page' do
-            get('/users/403')
-            expect(response.body).to render_template('show')  
-        end
+    it 'render index template' do
+      get('/users')
+      expect(response).to render_template('index')
     end
+
+    it 'the veiw body is correct (index)' do
+      get('/users')
+      expect(response.body).to include('<h1>index for users</h1>')
+    end
+  end
+
+  context 'the show method' do
+    it 'show specific user veiw' do
+      get('/users/403')
+      expect(response.body).to include('<h1>Here is a spesific user</h1>')
+    end
+
+    it 'render the correct page' do
+      get('/users/403')
+      expect(response.body).to render_template('show')
+    end
+  end
 end
