5ec4d5e9a8bb7d9160c0f9bafa7c90b0b18a904f
Alzubair98 <203721133@ogr.altinbas.edu.tr>
2022-10-26 22:05:23 +0300

    add views

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,10 +1,13 @@
 class PostsController < ApplicationController
   def show
     find_post
+    @comments = @post.most_recent_comments
+    @user = User.all
   end
 
   def index
     @posts = Post.all
+    @user = User.find(params[:user_id])
   end
 
   private
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,6 +5,7 @@ class UsersController < ApplicationController
 
   def show
     find_user
+    @posts = @user.last_3_posts
   end
 
   private
