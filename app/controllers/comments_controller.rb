38f6567507117d724f4d53c17cc23b2a0e627c11
Alzubair98 <203721133@ogr.altinbas.edu.tr>
2022-10-28 16:15:07 +0300

    fix linters

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,17 +1,13 @@
 class CommentsController < ApplicationController
-    def def new
+  def new; end
 
-    end
-    
-    def create 
-        comment = Comment.new(params.require(:comment).permit(:text))
-        @post = Post.find(params[:post_id])
-        comment.user_id = current_user.id 
-        comment.post_id = @post.id
-        if comment.save 
-            flash[:notice] = "Comment created successfully"
-            redirect_to "/users/#{current_user.id}/posts/#{@post.id}"
-        end
-    end
+  def create
+    @comment = Comment.new(params.require(:comment).permit(:text))
+    @post = Post.find(params[:post_id])
+    @comment.user_id = current_user.id
+    @comment.post_id = @post.id
+    flash[:notice] = 'Comment created successfully' if comment.save
 
+    redirect_to "/users/#{current_user.id}/posts/#{@post.id}"
+  end
 end
--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -14,17 +14,14 @@ class PostsController < ApplicationController
     @post = Post.new
   end
 
-  def create 
+  def create
     @post = Post.new(params.require(:post).permit(:title, :text))
     @post.user_id = current_user.id
-    @post.comments_counter = 0 
+    @post.comments_counter = 0
     @post.likes_counter = 0
-    if @post.save 
-      flash[:notice] = "Post created successfully"
-      redirect_to user_posts_path
-    end
+    flash[:notice] = 'Post created successfully' if @post.save
+    redirect_to user_posts_path
   end
-  
 
   private
 
