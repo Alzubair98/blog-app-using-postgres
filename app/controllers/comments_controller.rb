b0cfeef9128eacaa65f7e90640329ddbbd470128
Alzubair98 <203721133@ogr.altinbas.edu.tr>
2022-10-28 16:07:50 +0300

    fix comments form

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,18 +1,16 @@
 class CommentsController < ApplicationController
     def def new
-        
+
     end
     
     def create 
-        @comment = Comment.new(params.require(:comment).permit(:text))
+        comment = Comment.new(params.require(:comment).permit(:text))
         @post = Post.find(params[:post_id])
-        @comment.user_id = current_user.id 
-        @comment.post_id = @post.id
-        if @comment.save 
+        comment.user_id = current_user.id 
+        comment.post_id = @post.id
+        if comment.save 
             flash[:notice] = "Comment created successfully"
             redirect_to "/users/#{current_user.id}/posts/#{@post.id}"
-        else  
-            redirect_to users_path
         end
     end
 
