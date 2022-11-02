1723efbdeeb2b4ae3618cf305ad485ee23a68078
Alzubair98 <203721133@ogr.altinbas.edu.tr>
2022-11-02 13:03:54 +0300

    add user-index test

--- spec/views_test/user_index_spec.rb
+++ spec/views_test/user_index_spec.rb
@@ -14,4 +14,21 @@ RSpec.describe 'user_index', type: :feature do
   it 'shows the username of the users' do
     expect(page).to have_content("#{@user.name}")
   end
+
+  it 'shows the photo of the user' do 
+    expect(page.html).to include("user-img")
+  end
+
+  it 'shows the number of posts' do
+    expect(page).to have_content("#{@user.posts_counter}")
+  end
+
+  it 'should redirect to user show page when a name is clicked' do
+    user = User.last
+    click_link(user.name)
+    expect(page.current_path).to eql("/users/#{user.id}")
+  end
+
+  
+
 end
