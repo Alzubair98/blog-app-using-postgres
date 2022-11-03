cd228d399cd839dfd5f408ae3e8dd0a2af99bc52
Alzubair98 <203721133@ogr.altinbas.edu.tr>
2022-11-04 01:56:47 +0300

    fix bugs , give premistion to admin and users

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,4 +1,5 @@
 class CommentsController < ApplicationController
+ 
   def new; end
 
   def create
--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,5 @@
 class PostsController < ApplicationController
+  
   def show
     find_post
     @comments = @post.most_recent_comments
@@ -17,8 +18,6 @@ class PostsController < ApplicationController
   def create
     @post = Post.new(params.require(:post).permit(:title, :text))
     @post.user_id = current_user.id
-    @post.comments_counter = 0
-    @post.likes_counter = 0
     flash[:notice] = 'Post created successfully' if @post.save
     redirect_to user_posts_path
   end
