1aa25d00bfba53fad550fb0f56a8aa3ae65e6951
Alzubair98 <203721133@ogr.altinbas.edu.tr>
2022-11-03 15:35:32 +0300

    add device sing_in , sing_out and sing_up

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,5 @@
 class ApplicationController < ActionController::Base
+  before_action :authenticate_user!
   protect_from_forgery with: :exception 
 
   before_action :update_allowed_parameters, if: :devise_controller?
@@ -7,6 +8,6 @@ class ApplicationController < ActionController::Base
 
   def update_allowed_parameters
     devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:name, :email , :password, :bio, :photo)}
-    devise_parameter_sanitizer.permit(:account_update) { |u| u.permit(:name, :bio, :photo, :email , :password)}
+    devise_parameter_sanitizer.permit(:account_update) { |u| u.permit(:name, :email , :password, :current_password, :bio, :photo)}
   end
 end
