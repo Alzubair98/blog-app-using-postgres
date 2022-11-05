7d55cd6cca7ee07a3104a9a42d394e5e4e6a6f45
Alzubair98 <203721133@ogr.altinbas.edu.tr>
2022-11-05 20:48:38 +0300

    add swager

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,12 +1,12 @@
 # This file is copied to spec/ when you run 'rails generate rspec:install'
 require 'spec_helper'
 ENV['RAILS_ENV'] ||= 'test'
-require File.expand_path('../config/environment', __dir__)
+require_relative '../config/environment'
 # Prevent database truncation if the environment is production
-abort('The Rails environment is running in production mode!') if Rails.env.production?
+abort("The Rails environment is running in production mode!") if Rails.env.production?
 require 'rspec/rails'
 # Add additional requires below this line. Rails is not loaded until this point!
-require 'capybara/rspec'
+
 # Requires supporting ruby files with custom matchers and macros, etc, in
 # spec/support/ and its subdirectories. Files matching `spec/**/*_spec.rb` are
 # run as spec files by default. This means that files in spec/support that end
@@ -27,8 +27,7 @@ require 'capybara/rspec'
 begin
   ActiveRecord::Migration.maintain_test_schema!
 rescue ActiveRecord::PendingMigrationError => e
-  puts e.to_s.strip
-  exit 1
+  abort e.to_s.strip
 end
 RSpec.configure do |config|
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
--- spec/requests/api/v1/posts_spec.rb
+++ spec/requests/api/v1/posts_spec.rb
@@ -1,6 +1,24 @@
 require 'rails_helper'
 
 RSpec.describe 'Api::V1::Posts', type: :request do
+  before(:each) do
+        DatabaseCleaner.clean_with(:truncation)
+        @user = User.create(
+          name: 'zubair',
+          photo: 'https://unsplash.com/photos/F_-0BxGuVvo',
+          bio: 'full-stack developer.',
+        )
+    
+        @post1 = Post.new(title: 'firstpost', text: 'post one')
+        @post1.user_id = @user.id
+        @post1.save
+    
+        @comment1 = Comment.new(text: 'first comment')
+        @comment1.user_id = @user.id
+        @comment1.post_id = @post1.id
+        @comment1.save
+  end
+
   describe 'GET /index' do
     it 'returns http success' do
       get '/api/v1/posts/index'
@@ -10,7 +28,7 @@ RSpec.describe 'Api::V1::Posts', type: :request do
 
   describe 'GET /show' do
     it 'returns http success' do
-      get '/api/v1/posts/show'
+      get "/api/v1/posts/#{@post1.id}/show"
       expect(response).to have_http_status(:success)
     end
   end
